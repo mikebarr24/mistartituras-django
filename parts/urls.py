802b524945add85f1647fc5ede4e7f7c54f91819
Michael B <michaelbarr24@hotmail.com>
2022-07-08 12:54:08 +0200

    Add docker-compose and empty page structure

--- parts/urls.py
+++ parts/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('nosotros', views.nosotros, name="nosotros"),
+    path('buscar', views.buscar, name="buscar"),
+    path('contacto', views.contacto, name="contacto"),
 ]
--- parts/views.py
+++ parts/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "parts/index.html")
+
+
+def nosotros(request):
+    return render(request, "parts/nosotros.html")
+
+
+def buscar(request):
+    return render(request, "parts/buscar.html")
+
+
+def contacto(request):
+    return render(request, "parts/contacto.html")
